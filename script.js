1bce16e6ca9f7f33021fdd66ccac46d0b0f5ab27
Damien Levrault <damien.levrault@enseirb-matmeca.fr>
2025-01-30 16:32:07 +0100

    ajouts fonctionnalités

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 // const words_api_link = "https://random-word-api.herokuapp.com/word?";
-const words_api_link = "https://trouve-mot.fr/api/size";
+const words_api_link = "https://trouve-mot.fr/api/";
 
 const word_to_write = document.getElementById("word_to_write");
-const time = document.getElementById("chrono");
 const nb_words = document.getElementById("nb_words");
 const length_words = document.getElementById("length_words");
+const categoriy = document.getElementById("category_words");
 const screen = document.getElementById("screen");
 const buttonStart = document.getElementById("start");
 const inputZone = document.getElementById("input_keyboard");
 const score = document.getElementById("score");
-
+const timeChrono = document.getElementById("time")
+const time = document.getElementById("chrono");
 
 let nbWords = nb_words.value;
 let lenWord = length_words.value;
 let words = [];
 let isFirstWord = true;
 let isGameRunning = false;
-
+let nCategory = categoriy.value;
+let timeCurrentChrono = 3;
 
 buttonStart.addEventListener("click", startGame);
 inputZone.addEventListener("input", check);
@@ -30,13 +32,29 @@ nb_words.addEventListener("change", () => {
     nbWords = nb_words.value;
     get();
 });
+categoriy.addEventListener("change", () => {
+    words = [];
+    nCategory = categoriy.value;
+    get();
+})
 
 
 async function getWords() {
     try {
-        const response = await fetch(words_api_link + "/" + lenWord + "/" + nbWords);
-        const data = await response.json();
-        words.push(...data);
+        let response;
+        if (nCategory == 0) {
+            if (lenWord == 0) {
+                response = await fetch(words_api_link + "random/" + nbWords);
+            } else {
+                response = await fetch(words_api_link + "maxsize/" + lenWord + "/" + nbWords);
+            }
+            const data = await response.json();
+            words.push(...data);
+        }else{
+            response = await fetch(words_api_link + "categorie/" + nCategory + "/" + nbWords);
+            const data = await response.json();
+            words.push(...data);
+        }
     } catch (error) {
         console.error('Erreur:', error);
     }
@@ -49,14 +67,21 @@ function addWordToScreen(word){
 
 function changeEtat(){
     if(isGameRunning){
+        clearInterval(interval)
+        resetTimer()
         start.innerText = "Commencer"
         isGameRunning = false;
         score.innerText = "0";
-        screen.innerHTML = "";
         words = [];
-        clearInterval(interval)
+        timeOver = false;
         get();
+        screen.innerHTML = "";
+        time.removeAttribute("disabled");
+        nb_words.removeAttribute("disabled");
+        categoriy.removeAttribute("disabled");
+        length_words.removeAttribute("disabled");
     }else{
+        startTimer();
         isGameRunning = true;
         start.innerText = "Recommencer"
     }
@@ -64,6 +89,10 @@ function changeEtat(){
 }
 
 function startGame() {
+    time.setAttribute("disabled", "");
+    nb_words.setAttribute("disabled","");
+    categoriy.setAttribute("disabled", "");
+    length_words.setAttribute("disabled", "");
     changeEtat()
     if(isGameRunning){
         let wordsToPrint = [];
@@ -97,6 +126,49 @@ function check(){
 async function get() {
     await getWords();
     console.log(words);
+}
+
+function startTimer(){
+    timeCurrentChrono = 3;
+    timeChrono.textContent = timeCurrentChrono;
+    timerInterval = setInterval(() => {
+        timeCurrentChrono = timeCurrentChrono - 1;
+        if(timeCurrentChrono == 0){
+            timeCurrentChrono = 0;
+            stopTimer();
+        }
+        timeChrono.textContent = timeCurrentChrono;
+    }, 1000);
+    let data = { year: 2012 };
+
+    //////////////////////////////////////////////////////////////
+    // Convertir en JSON
+    const jsonString = JSON.stringify(data, null, 2);
+
+    // Créer un fichier et déclencher le téléchargement
+    const blob = new Blob([jsonString], { type: "application/json" });
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = "data.json";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    ////////////////////////////////////////////////////////////////
+    // faire un tableau des scores {} avec les elements à afficher sur la session. pas de serveur, tant pis
+}
+
+function stopTimer(){
+    console.log("stop timer")
+    console.log(timeCurrentChrono)
+    clearInterval(timerInterval);
+    clearInterval(interval);
+}
+
+function resetTimer(){
+    console.log("aaaaaaaaaaaaaaa");
+    clearInterval(timerInterval);
+    timeCurrentChrono = 3;
+    timeChrono.textContent = timeCurrentChrono;
 }
 
 get();
